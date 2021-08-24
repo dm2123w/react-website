2e26731742e620f3abd2b034562ae47dec8ea7e7
dmitriysemenko <dm123semenko@gmail.com>
2021-08-24 19:02:33 +0100

    update

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServisesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
     return (
         <>
             <AboutSection />;
             <ServicesSection />
+            <FaqSection />
         </>
     );
 };
