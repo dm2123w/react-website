a64cf53b12925e97fe848164be850945a409b252
dmitriysemenko <dm123semenko@gmail.com>
2021-08-24 20:08:15 +0100

    update

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,11 @@
 import React from "react";
 import home1 from "../img/home1.png";
+// Styled Components
+import styled from "styled-components";
 
 const AboutSection = () => {
     return (
-        <div>
+        <About>
             <div className="description">
                 <div className="title">
                     <div className="hide">
@@ -28,8 +30,18 @@ const AboutSection = () => {
             <div className="image">
                 <img src={home1} alt="Guy with a camera" />
             </div>
-        </div>
+        </About>
     );
 };
+
+// Styled Components
+const About = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
 
 export default AboutSection;
