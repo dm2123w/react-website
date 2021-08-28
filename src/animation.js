8f217b840de867c620c9caa5f208cc5d8cc78d3a
dmitriysemenko <dm123semenko@gmail.com>
2021-08-28 14:49:12 -0100

    update

--- src/animation.js
+++ src/animation.js
@@ -47,7 +47,24 @@ export const photoAnim = {
         opacity: 1,
         transition: {
             duration: 0.75,
-            ease: "easeOut"
-        }
-    }
+            ease: "easeOut",
+        },
+    },
+};
+
+export const lineAnim = {
+    hidden: { width: "0%" },
+    show: {
+        width: "100%",
+        transition: { duration: 1 },
+    },
+};
+
+export const slider = {
+    hidden: { x: "-130%", skew: "45deg" },
+    show: {
+        x: "100%",
+        skew: "0deg",
+        transition: { ease: "easeOut", duration: 1 },
+    },
 };
