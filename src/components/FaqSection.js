d0a8da0b17d66356120f51fd6f213717670e35a5
dmitriysemenko <dm123semenko@gmail.com>
2021-08-29 18:56:42 -0100

    update

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 const FaqSection = () => {
     return (
@@ -9,9 +10,8 @@ const FaqSection = () => {
             <h2>
                 Any Questions? <span>FAQ</span>
             </h2>
-            <Toggle>
-                <div className="question">
-                    <h4>How Do I Start?</h4>
+            <AnimateSharedLayout>
+                <Toggle title="How Do I Start?">
                     <div className="answer">
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>
@@ -19,12 +19,9 @@ const FaqSection = () => {
                             elit. Corporis, deleniti.
                         </p>
                     </div>
-                    <div className="faq-line"></div>
-                </div>
-            </Toggle>
-            <Toggle>
-                <div className="question">
-                    <h4>Daily Schedule</h4>
+                </Toggle>
+                <Toggle title="Daily Schedule">
+                    <h4></h4>
                     <div className="answer">
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>
@@ -32,12 +29,8 @@ const FaqSection = () => {
                             elit. Corporis, deleniti.
                         </p>
                     </div>
-                    <div className="faq-line"></div>
-                </div>
-            </Toggle>
-            <Toggle>
-                <div className="question">
-                    <h4>Different Payment Methods</h4>
+                </Toggle>
+                <Toggle title="Different Payment Methods">
                     <div className="answer">
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>
@@ -45,12 +38,8 @@ const FaqSection = () => {
                             elit. Corporis, deleniti.
                         </p>
                     </div>
-                    <div className="faq-line"></div>
-                </div>
-            </Toggle>
-            <Toggle>
-                <div className="question">
-                    <h4>What Products Do You Offer?</h4>
+                </Toggle>
+                <Toggle title="What Products Do You Offer?">
                     <div className="answer">
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>
@@ -58,9 +47,8 @@ const FaqSection = () => {
                             elit. Corporis, deleniti.
                         </p>
                     </div>
-                    <div className="faq-line"></div>
-                </div>
-            </Toggle>
+                </Toggle>
+            </AnimateSharedLayout>
         </Faq>
     );
 };
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
 
-const Toggle = ({ children }) => {
+const Toggle = ({ children, title }) => {
     const [toggle, setToggle] = useState(true);
     return (
-        <div onClick={() => setToggle(!toggle)}>{toggle ? children : ""}</div>
+        <motion.div
+            layout
+            className="question"
+            onClick={() => setToggle(!toggle)}
+        >
+            <motion.h4 layout>{title}</motion.h4>
+            {toggle ? children : ""}
+            <div className="faq-line"></div>
+        </motion.div>
     );
 };
 
