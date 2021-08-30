954cbab88c4b4cd62e0bfd82bec82edef3ddcdbd
dmitriysemenko <dm123semenko@gmail.com>
2021-08-30 17:52:45 -0100

    update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Nav = () => {
+    const { pathname } = useLocation();
     return (
         <StyledNav>
             <h1>
@@ -13,12 +15,29 @@ const Nav = () => {
             <ul>
                 <li>
                     <Link to="/">About Us</Link>
+                    <Line
+                        transition={{ duration: 0.75 }}
+                        initial={{ width: "0%" }}
+                        animate={{ width: pathname === "/" ? "50%" : "0%" }}
+                    />
                 </li>
                 <li>
                     <Link to="/work">Our Work</Link>
+                    <Line
+                        transition={{ duration: 0.75 }}
+                        initial={{ width: "0%" }}
+                        animate={{ width: pathname === "/work" ? "50%" : "0%" }}
+                    />
                 </li>
                 <li>
                     <Link to="/contact">Contact Us</Link>
+                    <Line
+                        transition={{ duration: 0.75 }}
+                        initial={{ width: "0%" }}
+                        animate={{
+                            width: pathname === "/contact" ? "50%" : "0%",
+                        }}
+                    />
                 </li>
             </ul>
         </StyledNav>
@@ -57,9 +76,21 @@ const StyledNav = styled.nav`
             justify-content: space-around;
             width: 100%;
         }
-        li{
+        li {
             padding: 0;
         }
+    }
+`;
+
+const Line = styled(motion.div)`
+    height: 0.3rem;
+    background: #23d997;
+    width: 0%;
+    position: absolute;
+    bottom: -80%;
+    left: 60%;
+    @media (max-width: 750px) {
+        left: 0%;
     }
 `;
 
